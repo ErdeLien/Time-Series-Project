f174c1f0e0aa3788fb266284bcd230cd210dfe8f
ErdeLien <erliao@ucsd.edu>
2023-12-24 14:32:06 -0500

    Notebook edited

--- Factor_py/config.py
+++ Factor_py/config.py
@@ -6,14 +6,15 @@ FACTOR_CONFIG = {
     "start_date":datetime.date(2021,11,1),
     "end_date":datetime.date(2023,9,1),
     "shape":(670,1,24),
-    "pnl_path":os.path.join(os.path.dirname(os.getcwd()),"Pnl"),
-    "alpha_path":os.path.join(os.path.dirname(os.getcwd()),"Factor"),
-    "cache_path":os.path.join(os.path.dirname(os.getcwd()),"1_h\\Cache"),
+    "pnl_path":os.path.join(os.path.dirname(os.getcwd()),"Time-Series-Project","Pnl"),
+    "alpha_path":os.path.join(os.path.dirname(os.getcwd()),"Time-Series-Project","Factor"),
+    "cache_path":os.path.join(os.path.dirname(os.getcwd()),"Time-Series-Project","1_h\\Cache"),
     "back_days":30,
     "fee":0.0005,
     "high_thre":1,
     "low_thre":-1,
-    "asset_ret_path":os.path.join(os.path.dirname(os.getcwd()),"Return_1h"),
+    #"asset_ret_path":os.path.join(os.path.dirname(os.getcwd()),"Return_1h"),
+    "asset_ret_path":os.path.join(os.path.dirname(os.getcwd()),"Time-Series-Project","Return_1h"),
     #换asset时需要修改
     "crypto":["ETHUSDT"]
 }
